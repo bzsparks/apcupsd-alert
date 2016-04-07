5a93c84a591342178bb33667dd7561919d14fa7e
Benjamin Sparks <bzsparks@gmail.com>
2016-04-07 12:57:01 -0500

    Cleanup all scripts
    Cleanup all scripts

---
+++ offbattery.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import smtplib
+import sqlite3
+import json
+from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+from contextlib import closing
+
+def GetRequired():
+    with open('required.json', 'r') as fin:
+        return json.loads(fin.read())
+
+def GetOutageInfo(required, now):
+    try:
+        con = sqlite3.connect('apcupsd.sqlite')
+        with closing(con.cursor()) as cur:
+            cur.execute('INSERT INTO apcd_offbattery (offbattery) VALUES (\'{0}\')'.format(now))
+            cur.execute('SELECT last_onbattery FROM apcd_last_onbattery WHERE rowid=1')
+            lastID = cur.fetchone()[0]
+            cur.execute('SELECT onbattery FROM apcd_onbattery WHERE rowid={0}'.format(lastID))
+            lastonbatt = cur.fetchone()[0]
+        con.commit()
+    except sqlite3.Error as e:
+        if con:
+            con.rollback()
+        print('Error {0}:'.format(e.args[0]))
+    finally:
+        if con:
+            con.close()
+
+    then = datetime.strptime(lastonbatt, '%Y-%m-%d %H:%M:%S.%f')
+    delta = (now - then)
+    
+    return str(timedelta(seconds=delta.seconds))
+
+def SendGmailMsg(required, now, downtime):
+    gmailAddress = required['sender']
+    gmailPassword = required['password'] #App Specific Password
+
+    fromSender = gmailAddress
+    toRecipients = required['recipients']
+
+    msg_subject = 'ALERT: Home UPS Power Restore'
+    msg_text = 'Home is now operating normally {0}. Power was off for {1}'.format(now,downtime)
+
+    msg = MIMEText(msg_text)
+    msg['Subject'] = msg_subject
+    msg['From'] = fromSender
+    msg['To'] = ", ".join(toRecipients)
+    s = smtplib.SMTP_SSL('smtp.gmail.com', '465')
+    s.login(gmailAddress, gmailPassword)
+    s.sendmail(fromSender, toRecipients, msg.as_string())
+    s.quit()
+
+#Main
+if __name__ == "__main__":
+    now = datetime.now()
+    required = GetRequired()
+    downtime = GetOutageInfo(required, now)
+    SendGmailMsg(required, now, downtime)
---
+++ onbattery.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import smtplib
+import sqlite3
+import json
+from datetime import datetime
+from email.mime.text import MIMEText
+from contextlib import closing
+
+def GetRequired():
+    with open('required.json', 'r') as fin:
+        return json.loads(fin.read())
+
+def SetOutageInfo(now):
+    try:
+        con = sqlite3.connect('apcupsd.sqlite')
+        with closing(con.cursor()) as cur:
+            cur.execute('INSERT INTO apcd_onbattery (onbattery) VALUES (\'{0}\')'.format(now))
+            cur.execute('SELECT ID FROM apcd_onbattery WHERE onbattery=\'{0}\''.format(now))
+            lastID = cur.fetchone()[0]
+            cur.execute('UPDATE apcd_last_onbattery SET last_onbattery=(\'{0}\') WHERE rowid=1'.format(lastID))
+        con.commit()
+    except sqlite3.Error as e:
+        if con:
+            con.rollback()
+        print('Error {0}:'.format(e.args[0]))
+    finally:
+        if con:
+            con.close()
+
+def SendGmailMsg(required, now):
+    gmailAddress = required['sender']
+    gmailPassword = required['password'] #App Specific Password
+
+    fromSender = gmailAddress
+    toRecipients = required['recipients']
+
+    msg_subject = 'ALERT: Home UPS Power Failure'
+    msg_text = 'Home is now on battery power. {0}'.format(now)
+
+    msg = MIMEText(msg_text)
+    msg['Subject'] = msg_subject
+    msg['From'] = fromSender
+    msg['To'] = ", ".join(toRecipients)
+    s = smtplib.SMTP_SSL('smtp.gmail.com', '465')
+    s.login(gmailAddress, gmailPassword)
+    s.sendmail(fromSender, toRecipients, msg.as_string())
+    s.quit()
+
+#Main
+if __name__ == "__main__":
+    now = datetime.now()
+    required = GetRequired()
+    SetOutageInfo(now)
+    SendGmailMsg(required, now)
+    
